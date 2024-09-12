96b9918e69337e7520c935f767f4f76647947644
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-09-12 23:04:04 +0530

    Create meeting-rooms-ii.py
    Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
    
     
    
    Example 1:
    
    Input: intervals = [[0,30],[5,10],[15,20]]
    Output: 2
    Example 2:
    
    Input: intervals = [[7,10],[2,4]]
    Output: 1
     
    
    Constraints:
    
    1 <= intervals.length <= 104
    0 <= starti < endi <= 106

---
+++ meeting-rooms-ii.py
@@ -0,0 +1,13 @@
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x:x[0])
+        rooms = []
+        heapq.heappush(rooms,intervals[0][1])
+        for i in range(1,len(intervals)):
+            if(intervals[i][0]>=rooms[0]):
+                heapq.heappop(rooms)
+            heapq.heappush(rooms,intervals[i][1])
+
+        return len(rooms)
+            
