3291e103d674f2eeae924be2aeba6ef9a4386447
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-09-14 22:54:49 +0530

    Update majority-element.py
    In O(1) space and O(N) time

--- majority-element.py
+++ majority-element.py
@@ -13,4 +13,20 @@ class Solution:
                 majority_ele=i
         return majority_ele
 
+//O(1) Space
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count=0
+        for i in nums:
+            if(count==0):
+                candidate = i
+                count+=1
+            elif(candidate==i):
+                count+=1
+            else:
+                count-=1
+        return candidate
+            
+
         
