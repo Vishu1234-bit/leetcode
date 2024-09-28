47dff37f5c1527a1448f4b771737d962c1389920
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-09-29 00:46:46 +0530

    Update check-if-array-is-sorted-and-rotated.py

--- check-if-array-is-sorted-and-rotated.py
+++ check-if-array-is-sorted-and-rotated.py
@@ -1,15 +1,10 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        original_nums = nums.copy()
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if(nums[i]>nums[j]):
-                    nums[i],nums[j] = nums[j],nums[i]
-        if(nums==original_nums):
-            return True
-        rotations = 1
-        while(rotations<len(original_nums)):
-            if(nums[rotations:len(nums)]+nums[0:rotations] == original_nums):
-                return True
-            rotations+=1
-        return False
+        n=len(nums)
+        countbreaks=0
+        for i in range(0,len(nums)):
+            if(nums[i]>nums[(i+1)%n]):
+                countbreaks+=1
+            if(countbreaks>1):
+                return False
+        return True
