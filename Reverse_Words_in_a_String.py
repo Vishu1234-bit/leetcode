a4f53eaf52db4f7d41edcbbb232b264b895d55fd
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-10-04 17:57:12 +0530

    Update Reverse_Words_in_a_String.py
    optimal solution

--- Reverse_Words_in_a_String.py
+++ Reverse_Words_in_a_String.py
@@ -4,16 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        words=[]
-        print(s.split(" "))
-        for i in s.split(" "):
-            if(len(i)!=0):
-                print("not space")
-                words.append(i)
-        reversed_s=""
-        for i in range(len(words)-1,0,-1):
-            if(len(words[i]) != 0):
-                reversed_s+=words[i]
-                reversed_s+=" "
-        reversed_s+=words[0]
-        return reversed_s
+        s=s.strip()
+        left=0
+        right = len(s)-1
+        temp=""
+        ans=""
+        while(left<=right and s[left]==" "):
+            left+=1
+        while(right>left and s[right]==" "):
+            right-=1
+        while(left<=right):
+            if(s[left]!=" "):
+                temp+=s[left]
+                print(temp)
+            elif(s[left]==" "):
+                if(temp):
+                    ans = temp+ " "+ans if ans else temp
+                temp=""
+            left+=1
+        if(ans!=""):
+            ans = temp+" "+ans
+        else:
+            ans=temp
+
+        return ans
