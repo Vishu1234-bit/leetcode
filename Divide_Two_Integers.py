d27078bc9a3b10100e84d137d43d3a281f71e89a
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-10-15 16:12:09 +0530

    Update Divide_Two_Integers.py
    crt solution

--- Divide_Two_Integers.py
+++ Divide_Two_Integers.py
@@ -1,5 +1,10 @@
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
         if((dividend>=0 and divisor<0) or (dividend<0 and divisor>=0)):
             sign = -1 
         else:
@@ -9,7 +14,18 @@ class Solution:
         if(divisor == 1):
             result = dividend
         else:
-            result = len(range(0,dividend-divisor+1,divisor))
+            result=0
+            while(dividend>=divisor):
+                temp,multiple=divisor,1
+                print(temp,multiple,temp<<1)
+                while(dividend>=(temp<<1)):
+                    temp<<=1
+                    multiple<<=1
+                    print("after shifts",temp,multiple)
+                dividend-=temp
+                result+=multiple
+                print(dividend,result)
+                print(dividend,result)
         if(sign == -1):
             result  = -result
         minusLimit = -(2**31)
