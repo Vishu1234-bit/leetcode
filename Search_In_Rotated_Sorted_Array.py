b67003c7d6d2dadf34d19f8a8f62f5e3dd59125c
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-10-15 20:34:17 +0530

    Update Search_In_Rotated_Sorted_Array.py
    binary search algorithm

--- Search_In_Rotated_Sorted_Array.py
+++ Search_In_Rotated_Sorted_Array.py
@@ -1,17 +1,25 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left=0
-        right=len(nums)-1
-        while(left<len(nums) and right>=0):
-            if(nums[left]==target):
-                return left
-            elif(nums[left]>target):
-                if(nums[right]==target):
-                    return right
-                elif(nums[right]<target):
-                    return -1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low=0
+        high=len(nums)-1
+        while(low<=high):
+            mid=(low+high)//2
+            if(nums[mid]==target):
+                return mid
+            elif(nums[low]<=nums[mid]):
+                if(target>=nums[low] and target<=nums[mid]):
+                    high=mid-1
                 else:
-                    right-=1
+                    low=mid+1
             else:
-                left+=1
+                if(target>=nums[mid] and target<=nums[high]):
+                    low=mid+1
+                else:
+                    high=mid-1
         return -1
+
