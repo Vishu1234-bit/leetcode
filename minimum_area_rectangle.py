5caeef6fa28af023938db06f99ed01d9b46f8490
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2024-10-18 16:20:00 +0530

    Update minimum_area_rectangle.py
    optimized

--- minimum_area_rectangle.py
+++ minimum_area_rectangle.py
@@ -1,18 +1,26 @@
-class Solution:
-    def minAreaRect(self, points: List[List[int]]) -> int:
-        points_set = set(map(tuple,points))
-        min_Area = float('inf')
-        for i in range(0,len(points)):
-            for j in range(i+1,len(points)):
-                x1,y1 = points[i][0],points[i][1]
-                x2,y2 = points[j][0],points[j][1]
-                if((x1!=x2 and y1!=y2)):
-                    if((x1,y2) in points_set and (x2,y1) in points_set):
-                        area = abs(x1-x2)*abs(y1-y2)
-                        min_Area = min(area,min_Area)
-        if(min_Area!=float('inf')):
-            return min_Area
-        else:
-            return 0      
-
-        
+class Solution(object):
+    def minAreaRect(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        minarea=float('inf')
+        xcoords = {}
+        for i in points:
+            x,y = i
+            if(x not in xcoords):
+                xcoords[x]=[]
+            xcoords[x].append(y)
+        lastx={}
+        for x in sorted(xcoords.keys()):
+            ylist = xcoords[x]
+            ylist.sort()
+            for i in range(len(ylist)):
+                for j in range(i+1,len(ylist)):
+                    y1,y2=ylist[i],ylist[j]
+                    if((y1,y2) in lastx):
+                        width = x-lastx[(y1,y2)]
+                        height = abs(y1-y2)
+                        minarea = min(minarea,height*width)
+                    lastx[(y1,y2)]=x
+        return minarea if minarea!=float('inf') else 0
