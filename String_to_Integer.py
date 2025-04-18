6f50910dee5389e1a8bf09b0dae8a3f759ca31fb
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-04-18 12:09:28 +0530

    Update String_to_Integer.py

--- String_to_Integer.py
+++ String_to_Integer.py
@@ -1,21 +1,28 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        resultingS = ""
-        for i in s:
-            if(i.isdigit() or (i == '+' and resultingS=="") or (i=='-'and resultingS=="")):
-                resultingS+=i
-            elif(i == ' ' and resultingS==""):
-                continue
-            elif((i.isalpha() and resultingS=="")):
-                return 0
-            elif((i.isalpha() and resultingS!="") or (i=="+" and resultingS!="") or (i=="-" and resultingS!="") or (i==' ' and resultingS!="") or i=='.'):
-                break
-        if(resultingS!='' and resultingS!='+' and resultingS!='-'):
-            if(int(resultingS)>2**31-1):
-                return 2**31-1
-            elif(int(resultingS)<(-2**31)):
-                return -2**31
+        result=0
+        index=0
+        sign =1
+        charRead = False
+        while(index<len(s)):
+            if(s[index]=='-' and not charRead):
+                sign=-1
+                index+=1
+                charRead=True
+            elif(s[index]=='+' and not charRead):
+                sign=1
+                index+=1
+                charRead = True
+            elif(s[index].isdigit()):
+                result=result*10+int(s[index])
+                charRead=True
+                index+=1
+            elif((s[index]==' ' or s[index]=='0') and not charRead):
+                index+=1
             else:
-                return int(resultingS)
-        else:
-            return 0
+                break
+        if(sign*result>=2**31):
+            return (2**31)-1
+        elif(sign*result<=(-2**31)):
+            return -2**31
+        return sign*result
