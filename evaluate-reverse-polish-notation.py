95e9546fa806c4e8b95feb7f4ed64d3e10729af7
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-04-11 12:58:18 +0530

    Create evaluate-reverse-polish-notation.py
    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
    
    Evaluate the expression. Return an integer that represents the value of the expression.
    
    Note that:
    
    The valid operators are '+', '-', '*', and '/'.
    Each operand may be an integer or another expression.
    The division between two integers always truncates toward zero.
    There will not be any division by zero.
    The input represents a valid arithmetic expression in a reverse polish notation.
    The answer and all the intermediate calculations can be represented in a 32-bit integer.
     
    
    Example 1:
    
    Input: tokens = ["2","1","+","3","*"]
    Output: 9
    Explanation: ((2 + 1) * 3) = 9
    Example 2:
    
    Input: tokens = ["4","13","5","/","+"]
    Output: 6
    Explanation: (4 + (13 / 5)) = 6
    Example 3:
    
    Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
    Output: 22
    Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
    = ((10 * (6 / (12 * -11))) + 17) + 5
    = ((10 * (6 / -132)) + 17) + 5
    = ((10 * 0) + 17) + 5
    = (0 + 17) + 5
    = 17 + 5
    = 22
     
    
    Constraints:
    
    1 <= tokens.length <= 104
    tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].

---
+++ evaluate-reverse-polish-notation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for char in tokens:
+            if(char=='-' or char=='+' or char=='*' or char=='/'):
+                a = stack.pop()
+                b= stack.pop()
+                if(char=='-'):
+                    stack.append(b-a)
+                elif(char=='+'):
+                    stack.append(a+b)
+                elif(char=='*'):
+                    stack.append(a*b)
+                else:
+                    stack.append(int(b/a))
+            else:
+                stack.append(int(char))
+        return stack[0]
