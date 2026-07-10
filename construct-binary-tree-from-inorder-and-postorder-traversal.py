ea86cc7da563d0ebaf647f63a395184a80591c06
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-07-10 16:41:40 +0530

    Create construct-binary-tree-from-inorder-and-postorder-traversal.py
    Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
    
     
    
    Example 1:
    
    
    Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
    Output: [3,9,20,null,null,15,7]
    Example 2:
    
    Input: inorder = [-1], postorder = [-1]
    Output: [-1]
     
    
    Constraints:
    
    1 <= inorder.length <= 3000
    postorder.length == inorder.length
    -3000 <= inorder[i], postorder[i] <= 3000
    inorder and postorder consist of unique values.
    Each value of postorder also appears in inorder.
    inorder is guaranteed to be the inorder traversal of the tree.
    postorder is guaranteed to be the postorder traversal of the tree.
     
    Seen this question in a real interview before?
    1/6

---
+++ construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        inorderMap = {val:i for i,val in enumerate(inorder)}
+        postorder_index=len(postorder)-1
+        def helper(left,right):
+            nonlocal postorder_index
+            if(left>right):
+                return None
+            root_val = postorder[postorder_index]
+            postorder_index-=1
+            root = TreeNode(root_val)
+            mid = inorderMap[root_val]
+            root.right = helper(mid+1,right)
+            root.left = helper(left,mid-1)
+            return root
+        return helper(0,len(inorder)-1)
